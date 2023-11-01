96096f7308e482e0bd4e00f30edb41939915a6f8
iankiplagat77 <145757359+iankiplagat77@users.noreply.github.com>
2023-11-01 09:24:45 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -21,11 +21,12 @@ document.addEventListener("DOMContentLoaded", () => {
         const films = document.getElementById('films');
   
         data.forEach(movie => {
-          films.insertAdjacentHTML('beforeend', `<li>${movie.title}</li>`)
+          films.insertAdjacentHTML('beforeend', `<li id="${movie.id}" onclick="clickmovie(this.id)">${movie.title}</li>`);
+
         });
           // display clicked movie
+        
           films.addEventListener('click', () => {
-            // display clicked movie
             fetch(`http://localhost:3000/films/${movie.id}`)
               .then(response => response.json())
               .then(movieData => {
@@ -45,6 +46,25 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById('availableTickets').innerHTML = data.capacity-data.tickets_sold;
     }
   });
+  function clickmovie(id){
+    fetch(`http://localhost:3000/films/` + id)
+          .then(response => response.json())
+          .then(movieData => {
+            filmdetails(movieData);
+          });
+  }
+
+  function filmdetails(data) {
+    document.getElementById('title').innerHTML = data.title;
+      document.getElementById('movie-url').src = data.poster;
+      document.getElementById('runtime').textContent = `${data.runtime} minutes`;
+      document.getElementById('showtime').textContent = data.showtime;
+      document.getElementById('description').textContent = data.description;
+      document.getElementById('availableTickets').innerHTML = data.capacity-data.tickets_sold;
+  }
+
+
+
   function purchase(){
     let available_tickets = document.getElementById('availableTickets').innerHTML;
     if (available_tickets > 0){
